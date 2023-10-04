76ef500c26c29e5f21f51f162cf8ca7fca16b5f3
Ara Mkrtchyan <mkrtchyanara22@gmail.com>
2023-10-04 21:05:20 +0400

    exampl5

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Enter your name:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("WOW! its Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
